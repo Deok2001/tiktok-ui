cc289dc0863d004c33e21a4965c8441a516783d3
Hoàng Anh Đức <hoanganhduczzxx@gmail.com>
2023-03-15 11:08:58 +0700

    Prettier

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import Button from '~/components/Button'
+import Button from '~/components/Button';
 
 function App() {
   return (
@@ -9,17 +9,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          Edit <code> src / App.js </code> and save to reload.{' '}
+        </p>{' '}
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React{' '}
+        </a>{' '}
+      </header>{' '}
     </div>
   );
 }
